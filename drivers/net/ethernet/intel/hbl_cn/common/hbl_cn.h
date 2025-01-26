306f1c907b38d1444c7c62b70c25af581dc92026
Tal Cohen <tal5.cohen@intel.com>
2025-01-26 15:51:14 +0200

    net: hbl_cn: create common cfg lock function
    For upstream purposes and to have more robust code, a common
    cfg lock function shall be created. The function shall invoke
    the asic port's specific function.
    
    Signed-off-by: Tal Cohen <tal5.cohen@intel.com>

--- drivers/net/ethernet/intel/hbl_cn/common/hbl_cn.h
+++ drivers/net/ethernet/intel/hbl_cn/common/hbl_cn.h
@@ -1630,6 +1630,8 @@ void hbl_cn_eq_handler(struct hbl_cn_port *cn_port);
 int hbl_cn_alloc_ring(struct hbl_cn_device *hdev, struct hbl_cn_ring *ring, int elem_size,
 		      int count);
 void hbl_cn_free_ring(struct hbl_cn_device *hdev, struct hbl_cn_ring *ring);
+void hbl_cn_cfg_lock(struct hbl_cn_port *cn_port);
+void hbl_cn_cfg_unlock(struct hbl_cn_port *cn_port);
 
 struct hbl_cn_user_cq *hbl_cn_user_cq_get(struct hbl_cn_port *cn_port, u8 cq_id);
 int hbl_cn_user_cq_put(struct hbl_cn_user_cq *user_cq);
